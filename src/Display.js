21ccd8a7b124ed4b221c18ef045c24bf2f038b94
Samuel Ramos <samramox@gmail.com>
2017-12-15 18:57:04 -0200

    Added grid and search

--- src/Display.js
+++ src/Display.js
@@ -1,15 +1,19 @@
 import React from 'react';
 
 const Display = ({todo, removeTodo, editTodo}) => (
-  <ul>
-    { todo.map((todo) => (
-        <li className="todo-task _flex _between _align-center" key={ todo.id }>
-          <input type="checkbox"></input>
-          <textarea className="text" maxLength="35" defaultValue={ todo.text } onChange={ this.editTodo }>
-          </textarea>
-          <button type="button" className="button -remove" onClick={ () => removeTodo(todo) }></button>
-        </li>)) }
-  </ul>
+  <div className="container">
+    <div className="row">
+      <ul className="col-12">
+        { todo.map((todo) => (
+            <li className="todo-task _flex _between _align-center" key={ todo.id }>
+              <input type="checkbox"></input>
+              <textarea className="text" maxLength="35" defaultValue={ todo.text } onChange={ this.editTodo }>
+              </textarea>
+              <button type="button" className="button -remove" onClick={ () => removeTodo(todo) }></button>
+            </li>)) }
+      </ul>
+    </div>
+  </div>
 )
 
 export default Display
--- src/Form.js
+++ src/Form.js
@@ -14,18 +14,25 @@ class Form extends Component {
     })
   }
 
-
   render() {
     const {submit} = this.props
+
     return (
       <div>
         <header className="todo-header">
           <h1>TODO LIST</h1>
         </header>
-        <form className="todo-form _flex _between _align-center" onSubmit={ (e) => submit(e) }>
-          <input type="text" maxLength="35" className="text" placeholder="Start typing..." onChange={ this.handleTextChange } value={ this.state.value } />
-          <button type="submit" className="button -add"></button>
+        <form className="todo-search _flex _align-center">
+          <input type="text" className="text" maxLength="35" placeholder="Start search..."/>
         </form>
+        <div className="container">
+          <div className="row">
+            <form className="todo-form col-12 _flex _between _align-center" onSubmit={ (e) => submit(e) }>
+              <input type="text" maxLength="35" className="text" placeholder="Start typing..." onChange={ this.handleTextChange } value={ this.state.value } />
+              <button type="submit" className="button -add"></button>
+            </form>
+          </div>
+        </div>
       </div>
     )
   }
