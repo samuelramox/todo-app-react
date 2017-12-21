44443d925c556ed6f37b42dc0ca787737edfd9e3
Samuel Ramos <samramox@gmail.com>
2017-12-21 14:27:34 -0200

    TodoList CSS refactor

--- src/components/todolist/index.js
+++ src/components/todolist/index.js
@@ -6,7 +6,7 @@ const TodoList = ({todos, removeTodo, editTodo}) => (
     <div className="row">
       <ul className="col-12">
         { todos.map((todos) => (
-            <li className="todo-task _flex _between _align-center" key={ todos.id }>
+            <li className="todo-list _flex _between _align-center" key={ todos.id }>
               <input type="checkbox" />
               <textarea className="text" maxLength="35" defaultValue={ todos.text } onChange={ this.editTodo }>
               </textarea>
